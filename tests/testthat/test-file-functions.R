af77d1c309061e90ed9763375d3695a5c586d2b1
Lee Zeman <lzeman@uvic.ca>
2025-04-10 15:38:59 -0700

    remove data copy

--- tests/testthat/test-file-functions.R
+++ tests/testthat/test-file-functions.R
@@ -155,47 +155,6 @@ test_that("data can be written to a netCDF file", {
 	nc.copy.atts(f.in, "rlon", f.out, "rlon")
 	nc.copy.atts(f.in, "tasmax", f.out, "tasmax")
 	
-	# this section is a copy-paste inlining of nc.put.var.subst.by.axes to find the error
-	axis.indices <- list()
-	axes.map <- NULL
-	input.axes <- NULL
-	
-	if(is.null(axes.map))
-      axes.map <- nc.get.dim.axes(f.out, "tasmax")
-
-    if(length(axes.map) == 0)
-      return(c())
-
-    ### Permute data to match order within file...
-    if(!is.null(input.axes)) {
-      stopifnot(length(dim(dat)) == length(input.axes))
-      stopifnot(length(input.axes) == length(axes.map))
-      o.axes <- order(axes.map)
-      o.input <- order(input.axes)
-      if(o.axes != o.input)
-        dat <- aperm(dat, o.axes[o.input])
-    }
-  
-    ## Check that all axes are in the map and that the names are the same as the dim names
-    stopifnot(all(names(axis.indices) %in% axes.map))
-    stopifnot(names(axes.map) %in% nc.get.dim.names(f.out, "tasmax"))
-  
-    ## Chunk consecutive sets of blocks into a request
-    chunked.axes.indices <- lapply(axis.indices, function(indices) {
-      if(length(indices) == 0) return(c())
-      boundary.indices <- c(0, which(diff(indices) != 1), length(indices))
-      lapply(1:(length(boundary.indices) - 1), function(x) { (indices[boundary.indices[x] + 1]):indices[boundary.indices[x + 1]] } )
-    })
-  
-    ### By default, fetch all data.
-    starts <- rep(1, length(f.out$var[["tasmax"]]$dim))
-    counts <- rep(-1, length(f.out$var[["tasmax"]]$dim))
-    names(starts) <- names(counts) <- axes.map
-  
-    nc.put.subset.recursive(chunked.axes.indices, f.out, "tasmax", dat, starts, counts, axes.map)
-	
-	# end inlining of nc.put.var.subst.by.axes
-	
 	
 	nc_sync(f.out)
 	nc_close(f.out)
