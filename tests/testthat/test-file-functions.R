a67d348bb6b9bdb87326a5e2535e4812df1440d2
Lee Zeman <lzeman@uvic.ca>
2025-04-10 17:58:13 -0700

    skip units and cell_method

--- tests/testthat/test-file-functions.R
+++ tests/testthat/test-file-functions.R
@@ -151,7 +151,7 @@ test_that("data can be written to a netCDF file", {
 	
 	#copy attributes from another file
 	f.in <- nc_open("test1.nc")
-	nc.copy.atts(f.in, "tasmax", f.out, "tasmax", c("long_name", "standard_name" ))
+	nc.copy.atts(f.in, "tasmax", f.out, "tasmax", c("long_name", "standard_name", "units", "cell_methods"))
 	
     ncvar_put(f.out, "tasmax", dat)
 	
